115e65e6e68e57171b68b0c3c42d18b26ea2f780
huo223gg <44153293+huo223gg@users.noreply.github.com>
2018-12-14 16:23:46 +0900

    Revert "가입 시 메일 중복 확인"
    This reverts commit fe164cd1c5604797ed8c18639efbf1b99d54a7e7.

--- java-web/Lively/src/com/quadcore/lively/controller/MemberController.java
+++ java-web/Lively/src/com/quadcore/lively/controller/MemberController.java
@@ -44,10 +44,7 @@ public class MemberController {
 			return service.searchUserFromUserMail(userMail);
 			
 		}
-		public int registerCheck(String userMail) {
-			// TODO Auto-generated method stub
-			return service.registerCheck(userMail);
-		}
+
 	
 
 	
--- java-web/Lively/src/com/quadcore/lively/service/MemberService.java
+++ java-web/Lively/src/com/quadcore/lively/service/MemberService.java
@@ -65,10 +65,7 @@ public class MemberService{
 		String sql ="select * from member where usermail="+userMail;
 		return dao.st_execute(sql);
 	}
-	public int registerCheck(String userMail) {
-		// TODO Auto-generated method stub
-		return dao.getUserMail(userMail);
-	}
+
 
 
 }
