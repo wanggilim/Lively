22296abbc3bcfbf13bb7c15b1dcd5a595fd540aa
huo223gg <44153293+huo223gg@users.noreply.github.com>
2018-12-14 11:27:39 +0900

    머지 안정화 작업

--- java-web/Lively/src/com/quadcore/lively/model/MemberDao.java
+++ java-web/Lively/src/com/quadcore/lively/model/MemberDao.java
@@ -12,88 +12,96 @@ import java.util.Map;
 
 import com.quadcore.lively.util.OracleDBUtil;
 
-
-
-
-
-
 public class MemberDao{
 	
-	
-	
-
 	//멤버 추가
 	public void insert(MemberVO member) {
 		// TODO Auto-generated method stub
 		
 		String sql = "insert into member(userNo, userMail, userPass, gender, birthday) values(userNo.nextval,?,?,?,?)";
 		PreparedStatement st = null;
-		int result =0;
+		Connection conn = null;
+		ResultSet rs = null;
+	    int result =0;
 		
 		try {
-			Connection conn = OracleDBUtil.dbConnect();
+			conn = OracleDBUtil.dbConnect();
+			conn.setAutoCommit(false);
 			st = conn.prepareStatement(sql);
 		
 			st.setString(1, member.getUserMail());
 			st.setString(2, member.getUserPass());
 			st.setString(3, member.getGender());
 			st.setDate(4, member.getBirthday());
-	
 			
 			result=st.executeUpdate();
-
+			conn.commit();
 		}catch(SQLException e) {
 			e.printStackTrace();
+		}finally {
+			OracleDBUtil.dbDisconnect(rs, st, conn);
 		}
 		
-		
 	}
+	
 	//멤버조회
 	public MemberVO select(String userMail) {
 		// TODO Auto-generated method stub
 	
-		ResultSet rs = null;
-		//MemberVO m = null;
+		MemberVO member = null;
 		
 		String sql = "select * from member where userMail =?";	
 		PreparedStatement st = null;
+		Connection conn = null;
+		ResultSet rs = null;
 		
 		try {
-			Connection conn = OracleDBUtil.dbConnect();
+			conn = OracleDBUtil.dbConnect();
+			conn.setAutoCommit(false);
 			st = conn.prepareStatement(sql);
 			st.setString(1, userMail);
 			rs= st.executeQuery();
 			
 			if(rs.next()) {
-				return new MemberVO(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getString(5), rs.getDate(6));
+				member = new MemberVO(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getString(5), rs.getDate(6));
 			}
+			conn.commit();
 		}catch(SQLException e) {
 			e.printStackTrace();
+		}finally {
+			OracleDBUtil.dbDisconnect(rs, st, conn);
 		}
 		
-		return null;
+		return member;
 	}
+	
 	//멤버 수정
 	public void update(MemberVO member) {
 		// TODO Auto-generated method stub
 		
 		String sql = "update member set userNo=?, userPass=?,userLevel=?, gender=?, birthday=sysdate where userMail=?";
 		PreparedStatement st = null;
+		Connection conn = null;
+		ResultSet rs = null;
 		
 		try {
-			Connection conn = OracleDBUtil.dbConnect();
+			conn = OracleDBUtil.dbConnect();
+			conn.setAutoCommit(false);
 			st=conn.prepareStatement(sql);
 			st.setInt(1, member.getUserNo() );
-			st.setString(6, member.getUserMail());
 			st.setString(2, member.getUserPass());
 			st.setInt(3, member.getUserLevel());
 			st.setString(4, member.getGender());
 			st.setDate(5, member.getBirthday());
+			st.setString(6, member.getUserMail());
 			
 			st.executeUpdate();
+			conn.commit();
 					
 		}catch(SQLException e) {
 			e.printStackTrace();
+		}finally {
+			OracleDBUtil.dbDisconnect(rs, st, conn);
 		}
 	}
 
@@ -103,37 +111,54 @@ public class MemberDao{
 		
 		String sql = "delete member where userMail=?";
 		PreparedStatement st = null;
+		Connection conn = null;
+		ResultSet rs = null;
+		
 		try {
-			Connection conn = OracleDBUtil.dbConnect();
+			conn = OracleDBUtil.dbConnect();
+			conn.setAutoCommit(false);
 			st = conn.prepareStatement(sql);
 			st.setString(1, userMail);
 			st.executeUpdate();
-			
+			conn.commit();
 		}catch(SQLException e) {
 			e.printStackTrace();
+		}finally {
+			OracleDBUtil.dbDisconnect(rs, st, conn);
 		}
-		}
+		
+	}
+	
 	//user 가입을 위해 기존 가입한 userMail 존재 확인
-		public int getUserMail(String userMail) {
-			int count = 0;
-			
-			String sql = "SELECT count(*) FROM member"
-					+ " WHERE userMail = '" + userMail + "'";
-			try {
-				Connection conn = OracleDBUtil.dbConnect();
-				Statement st = conn.createStatement();
-				
-				ResultSet rs = st.executeQuery(sql);
-				while (rs.next()) {
-					count = rs.getInt(1); // 0번은 count(*)
-				}
-				
-			} catch (SQLException e) {
-				e.printStackTrace();
+	public int getUserMail(String userMail) {
+		int count = 0;
+		
+		String sql = "SELECT count(*) FROM member"
+				+ " WHERE userMail = '" + userMail + "'";
+		Connection conn=null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		
+		try {
+			conn = OracleDBUtil.dbConnect();
+			conn.setAutoCommit(false);
+			st = conn.createStatement();
+		    rs = st.executeQuery(sql);
+			while (rs.next()) {
+				count = rs.getInt(1); // 0번은 count(*)
 			}
-			
-			return count;
+			conn.commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			OracleDBUtil.dbDisconnect(rs, st, conn);
 		}
+		
+		return count;
+	}
+		
+		
 	//user 로그인 승인을 위해 존재 확인
 	public int getUserAuth(String userMail, String userPass) {
 		int count = 0;
@@ -141,17 +166,27 @@ public class MemberDao{
 		String sql = "SELECT count(*) FROM member"
 				+ " WHERE userMail = '" + userMail + "'" // 18
 				+ " AND userPass = '" + userPass + "'"; // 160
-		try {
-			Connection conn = OracleDBUtil.dbConnect();
-			Statement st = conn.createStatement();
-			
-			ResultSet rs = st.executeQuery(sql);
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {			
+			conn = OracleDBUtil.dbConnect();
+			conn.setAutoCommit(false);
+			st = conn.createStatement();
+			rs = st.executeQuery(sql);
 			while (rs.next()) {
+		
 				count = rs.getInt(1); // 0번은 count(*)
 			}
+			conn.commit();
 			
 		} catch (SQLException e) {
-			e.printStackTrace();
+			if(conn!=null) 
+				try {conn.rollback();} 
+				catch(SQLException e2) {}
+			
+		} finally {
+			OracleDBUtil.dbDisconnect(rs, st, conn);
 		}
 		
 		return count;
@@ -164,16 +199,15 @@ public class MemberDao{
 		Statement st =null;
 		ResultSet rs =null;
 		int result =0;	
-
-
 		
-		conn = OracleDBUtil.dbConnect();
 		System.out.println("오라클 접속");
 		System.out.println(sql);
 		try {
-			 st = conn.createStatement();
-			 result = st.executeUpdate(sql);
-			
+			conn = OracleDBUtil.dbConnect();
+			conn.setAutoCommit(false);
+			st = conn.createStatement();
+			result = st.executeUpdate(sql);
+			conn.commit();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}finally {
@@ -185,27 +219,32 @@ public class MemberDao{
 	
 	//userLevel 조회
 	public MemberVO getUserLevel(String userMail) {
-		ResultSet rs = null;
-		//MemberVO m = null;
+		MemberVO member = null;
 		
 		String sql = "select * from member where usermail =?";	
 		PreparedStatement st = null;
-		
-		try {
-			Connection conn = OracleDBUtil.dbConnect();
+		Connection conn = null;
+		ResultSet rs = null;
+		try {			
+			conn = OracleDBUtil.dbConnect();
+			conn.setAutoCommit(false);
 			st = conn.prepareStatement(sql);
 			st.setString(1, userMail);
-			rs= st.executeQuery();
+			rs = st.executeQuery(sql);
 			
 			if(rs.next()) {
-				return new MemberVO(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getString(5), rs.getDate(6));
+				member = new MemberVO(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getString(5), rs.getDate(6));
 			}
-		}catch(SQLException e) {
+			conn.commit();
+		} catch (SQLException e) {
 			e.printStackTrace();
+		}finally {
+			OracleDBUtil.dbDisconnect(rs, st, conn);
 		}
-		
-		return null;
+			
+		return member;
 	}
-	}
+	
+}
 
 
