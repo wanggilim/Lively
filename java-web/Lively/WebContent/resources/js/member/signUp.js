c5783c7bc77678455b4b7ee5251355c1a1e752d9
김휘중 <gnlwnd352@naver.com>
2018-12-20 20:34:12 +0900

    가입 에러 수정

--- java-web/Lively/WebContent/resources/js/member/signUp.js
+++ java-web/Lively/WebContent/resources/js/member/signUp.js
@@ -8,17 +8,15 @@
  * 2. 회원아이디가 중복되었는가?
  * 3. 비밀번호 입력조건이 유효하는가?
  */
-
 //중복 체크 Ajax
 function validateEmail(){
-	 alert("cc");
+
 	var userMail = document.getElementById("userMail").value;
 	var patternM =  /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-
+	var result;
 	if( patternM.test(userMail) ) {
 		//alert("메일 주소가 올바르게 입력되었습니다.");
 		// 중복 체크
-		
 		$.ajax({
 			type:'POST',
 			url:'./userMailDuplication.do',	
@@ -28,15 +26,15 @@ function validateEmail(){
 				// jquery ajax 처리(중복 체크) 에 대한 결과
 				if (data >= 1) {
 					alert('사용할 수 없는 이메일입니다.');
-					return false;
+					result = false;
 				} else {
 					alert('사용할 수 있는 이메일입니다.');
-					return true;
+					result = true;
 				}
 			},
 			error: function(xhr, status, error) {
 				alert('관리자에게 문의해주세요.');
-				return false;
+					result = false;
 			}
 		});
 
@@ -44,20 +42,20 @@ function validateEmail(){
 	} else {
 		alert("메일 주소가 유효하지 않습니다.");
 		document.getElementById("userMail").focus();//입력이 잘못된 곳에 커서 위치
-		return false; // submit이 가지 않고 머물도록 지정
+		result = false; // submit이 가지 않고 머물도록 지정
 	}
+	return result;
 }
-
 
 //메일(validateEmail)과 비밀번호에 대한 유효성 확인
 function registerCheckFunction() {
-	 alert("aa");
+
 	var userPass = document.getElementById("userPass").value;
 	var patternP = /^[A-Za-z0-9.+!@#]{8,15}$/;
-	
+
 	var idCheck = validateEmail();
-	
-	if (idCheck == false) {
+
+	if (idCheck == false ) {
 		console.log('이메일 검증 실패!');
 		return false;
 	}
@@ -74,7 +72,7 @@ function registerCheckFunction() {
 //나의 정보 수정 비밀번호 검증 
 function registerCheckUserpass(){
 	   
-    alert("bb");
+  
 	var userPass = document.getElementById("userPass").value;
 	var patternP = /^[A-Za-z0-9.+!@#]{8,15}$/;
 	
